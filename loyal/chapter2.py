7c9bf8f71b39c319cc39686ce08a2c2c544c85eb
Jinhee Jeon <wlsl6569@naver.com>
2025-12-03 11:12:58 +0900

    loyal added

--- loyal/chapter2.py
+++ loyal/chapter2.py
@@ -1,7 +1,7 @@
 import import_character
 from item_add import add_item 
 import time, random
-from farmer.chapter3 import start3
+from loyal.chapter3 import start3
 
 name, job, item = import_character.load_character()
 
@@ -9,28 +9,29 @@ name, job, item = import_character.load_character()
 print(item)
 
 '''
-print('농사 방법 : 오전에는 W키를 눌러 광합성을, 
-오후에는 A키를 눌러 물을 주고 저녁에는 D키를 눌러 삽으로 땅을 파헤쳐줘. 
-마지막으로 삽이 망가지면 S키를 눌러 삽을 수리해봐! ')
+ print('놀이 방법 : 오전에는 W키를 눌러 보석을 사들여. 
+ 오후에는 A키를 눌러 폭식을 하고 저녁에는 D키를 눌러 술을 마셔. 
+ 마지막으로 자괴감이 올라오면 S키를 눌러 기부를 해봐! ')
+        
 '''
 time_zone = {'morning': ["W", 'w'],'noon':['A', 'a'],'afternoon':['D', 'd'],'broke':['S', 's']}
-item = '감자'
+item = '우울증'
 
 def display_timezone(zone):
     if zone == 'morning':
-        print(f'{name}: 햇빛 무럭무럭 빔~~~~~~.')
+        print(f'{name}: 보석이 반짝반짝~~~~~~.')
     elif zone == 'noon':
-        print(f'{name}: 시원하게 물을 쫘아아악~~~~~.')
+        print(f'{name}: 양고기, 포도, 연어 모두 맛있다~~~~~.')
     elif zone == 'afternoon':
-        print(f'{name}: 흙아 송송 숨을 쉬거라~~~~~.')
+        print(f'{name}: 와인을 더 내놓거라~~~~~.')
     elif zone == 'broke':
-        print(f'{name}: 뚝딱뚝딱 수리 완료~~~~~.')
+        print(f'{name}: 흐흑, 나도 좋은 사람이지?')
 
 rounds = 5
 time_limit = 2
 
 def start2():
-    print(f'{name}: 좋아 농사 시작이다! 우하하하하항')
+    print(f'{name}: 좋아 귀족의 일상 시작이다! 우하하하하항')
 
     for i in range(rounds):
         print(f'============={i+1} / {rounds}===========')
@@ -46,23 +47,23 @@ def start2():
             time.sleep(1)
             print(f'{name}:....너무 늦었어.')
             time.sleep(1)
-            print('감자가 시들어버렸다!')
+            print('흥이 죽어버렸다!')
             return
 
         elif key not in time_zone[zone]:
             time.sleep(1)
             print(f'{name}: 으아악 실수 실수')
             time.sleep(1)
-            print('감자가 시들어버렸다!')
+            print('흥이 죽어버렸다!')
             return
         
         else:
             time.sleep(1)
-            print('감자가 자랐다!')
+            print('흥이 난다!')
 
 
     time.sleep(1)
-    print(f"{name}: 우왓~ 감자 풍년~~~~")
+    print(f"{name}: 우왓~ 인생은 즐거워~~~~")
     time.sleep(1)
     add_item(item)
     start3()
--- loyal/chapter3.py
+++ loyal/chapter3.py
@@ -8,9 +8,9 @@ name, job, item = import_character.load_character()
 
 def start3():
     time.sleep(1)
-    print(f'...고요한 저녁, {name}은 모닥불을 바라보며 앉아있다.')
+    print(f'...다음 날 연회장, {name}은 친구들에게 둘러쌓여 있다.')
     time.sleep(1)
-    print(f'{name}: 열심히 일했더니 출출하군.')
+    print(f'{name}: 친구들 앞에서 꺼내놓고 싶어졌다.')
     time.sleep(1)
 
     items, thing = use_item()
@@ -18,28 +18,34 @@ def start3():
     
 
     time.sleep(1)
-    print(f'모닥불에 {thing}를 넣었다.')
+    print(f'{name}은 친구들 앞에 {thing}를 꺼내놓는다.')
     time.sleep(1)
-    print(f'{name}는 어쩐지 눈물이 났다.')
+    print(f'{name}: 오래전부터 이걸 보여주고 싶었지.')
     time.sleep(1)
-    print(f'{name}: 이건 슬픔의 눈물이 아니야.')
+    print(f'친구들이 놀란 눈으로 {name}을 바라본다.')
     time.sleep(1)
-    print(f'{name}는 뜨거운 {thing}를 잡아들어 온기를 느낀다.')
-    time.sleep(1)
-    print(f'{name}: 이게 바로 내 힘으로 이뤄낸 식사구나.')
-    time.sleep(1)
-    if thing == '감자':
-        print(f'{thing}를 먹기 시작한다.')
-        print(f'내일도 또 다른 하루를 기다리게 하는 맛이다.')
+    if thing == '와인':
+        print(f'친구들 : 우와앗, 이건 최고급 {thing}...!')
+        print(f'{name}: 맞아. 너희와 먹는다면 그 순간만큼은 즐겁겠지..')
         time.sleep(1)
         print(f'...')
         time.sleep(1)
         print(f'The End')
         return
-    elif thing == '삽':
-        print(f'{thing}을 억지로 먹다가 {name}은 쓰러졌다!')
+    elif thing == '우울증':
+        print(f'친구들: 언제부터 이렇게 슬펐던거냐고!')
         time.sleep(1)
         print(f'...')
+        time.sleep(1)
+        print(f'{name}: 크흐흑, 사실은 그게...')
+        time.sleep(1)
+        print(f'...')
+        time.sleep(1)
+        print(f'{name}은 깊은 속얘기들을 시작했다.')
+        time.sleep(1)
+        print(f'{name}은 오랜만에 안정감을 느꼈다.')
+        time.sleep(1)
+        print('...')
         time.sleep(1)
         print(f'The End')
         return
